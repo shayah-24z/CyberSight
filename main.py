062dc5948b282c0b367af8ed0739cfcbe95c0932
shayah-24z <132129961+shayah-24z@users.noreply.github.com>
2025-07-13 10:56:33 +0100

    added user function

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sqlite3
+import bcrypt
+
 def show_menu():
     print("\n=== Secure User System ===")
     print("1. Register")
@@ -8,8 +11,26 @@ while True:
     show_menu()
     choice = input("Select an option (1-3): ")
 
-    if choice == "1":
-        print(" Registering user...")
+    if choice == "1": #register user function
+        
+        username = input("Enter a username: ")
+        email = input("Enter your email: ")
+        password = input("Enter a password: ")
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+        #connects to database
+        conn = sqlite3.connect("secure_app.db")
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute("INSERT INTO users (username, email, password) VALUES (?, ?, ?)", 
+                    (username, email, hashed_password))
+            conn.commit()
+            print(" User registered successfully!")
+        except sqlite3.IntegrityError:
+            print(" That username or email already exists.")
+        finally:
+            conn.close()
        
     elif choice == "2":
         print(" Logging in...")
